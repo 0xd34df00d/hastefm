2cf56a3094fbfd8ed5663ca876be0211c7051ee1
0xd34df00d <0xd34df00d@gmail.com>
2017-01-01 01:34:28 -0500

    Handler for "photos/parsepage".

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,15 @@ module Main where
 
 import qualified Snap.Core as S
 import qualified Snap.Http.Server as S
-import qualified Snap.Http.Server.Config as S
+import qualified Snap.Util.FileUploads as S
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Aeson as A
-import Data.Either
+import qualified System.IO.Streams as IS(read)
+import Data.Monoid
+
 import Lastfm.Photos
 
 main :: IO ()
@@ -24,7 +28,16 @@ pageUrlHandler = do
     maybe (S.writeBS "must specify the artist") S.writeLBS $ runOnText artistPhotoPage <$> artist
 
 parsePageHandler :: S.Snap ()
-parsePageHandler = undefined
+parsePageHandler = do
+    files <- S.handleMultipart S.defaultUploadPolicy $ const reader
+    case files of
+        [file] -> S.writeLBS $ runOnText parsePage file
+        _ -> S.writeBS "must specify the contents"
+    where reader is = do
+                maybeStr <- IS.read is
+                case maybeStr of
+                    Nothing -> pure mempty
+                    Just str -> (str <>) <$> reader is
 
 runOnText :: A.ToJSON a => (T.Text -> a) -> BS.ByteString -> BSL.ByteString
 runOnText f = either (BSL.pack . show) (A.encode . f) . T.decodeUtf8'
