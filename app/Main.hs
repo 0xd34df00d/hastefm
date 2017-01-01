52bc258ff7fb8c5c42b9422183b4bf13893759af
0xd34df00d <0xd34df00d@gmail.com>
2017-01-01 00:40:53 -0500

    Some initial stub handler for the artist path.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Snap.Core as S
+import qualified Snap.Http.Server as S
+import qualified Snap.Http.Server.Config as S
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text.Encoding as T
+import qualified Data.Aeson as A
+import Data.Either
 import Lastfm.Photos
 
 main :: IO ()
-main = undefined
+main = S.httpServe (S.setPort 12000 S.defaultConfig) site
+
+site :: S.Snap ()
+site = S.route [("photos/pageurl", pageUrlHandler),
+                ("photos/parsepage", parsePageHandler)]
+
+pageUrlHandler :: S.Snap ()
+pageUrlHandler = do
+    artist <- S.getPostParam "artist"
+    maybe (S.writeBS "must specify the artist") S.writeLBS $ handleArtist <$> artist
+    where handleArtist = either (BSL.pack . show) (A.encode . artistPhotoPage) . T.decodeUtf8'
+
+parsePageHandler :: S.Snap ()
+parsePageHandler = undefined
