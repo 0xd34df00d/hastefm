12b144e358f64d8554bbd2a720e122a1189efe73
0xd34df00d <0xd34df00d@gmail.com>
2017-12-15 13:37:03 -0500

    Artist recommendation data types.

--- src/Lastfm/Recommended.hs
+++ src/Lastfm/Recommended.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 
 module Lastfm.Recommended
         (
@@ -7,8 +8,17 @@ module Lastfm.Recommended
         where
 
 import qualified Data.Text as T
+import qualified Data.Aeson as A
+import GHC.Generics
 
 import Lastfm.FetchPageUrl
 
 recommendedArtistsPage :: T.Text -> FetchPageUrl
 recommendedArtistsPage = const FetchPageUrl { url = "https://www.last.fm/home/artists", requiresLogin = True }
+
+data Recommended = Recommended {
+                       name :: T.Text,
+                       similarTo :: [T.Text]
+                   } deriving (Eq, Show, Generic, A.ToJSON)
+
+newtype RecommendedArtists = RecommendedArtists { recommendedArtists :: [Recommended] } deriving (Eq, Show, Generic, A.ToJSON)
