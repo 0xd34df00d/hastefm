063abc40727f9a92cd2fde776c595632bdd263fa
0xd34df00d <0xd34df00d@gmail.com>
2017-12-15 14:34:41 -0500

    Recommended artists page parser.

--- src/Lastfm/Recommended.hs
+++ src/Lastfm/Recommended.hs
@@ -1,15 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+{-# LANGUAGE Arrows #-}
 
 module Lastfm.Recommended
         (
-         recommendedArtistsPage
+         recommendedArtistsPage,
+         parseArtistsPage
         )
         where
 
 import qualified Data.Text as T
 import qualified Data.Aeson as A
 import GHC.Generics
+import Text.XML.HXT.Core
+import Text.HandsomeSoup
 
 import Lastfm.FetchPageUrl
 
@@ -22,3 +26,12 @@ data Recommended = Recommended {
                    } deriving (Eq, Show, Generic, A.ToJSON)
 
 newtype RecommendedArtists = RecommendedArtists { recommendedArtists :: [Recommended] } deriving (Eq, Show, Generic, A.ToJSON)
+
+parseArtistsPage :: T.Text -> RecommendedArtists
+parseArtistsPage = RecommendedArtists . runLA recArtistsArrow . T.unpack
+
+recArtistsArrow :: ArrowXml a => a String Recommended
+recArtistsArrow = hread >>> css ".recs-feed-item--artist" >>> proc x -> do
+    name <- css ".link-block-target" /> getText >>> arr T.pack -< x
+    similarTo <- listA $ css ".context >> a" /> getText >>> arr T.pack -< x
+    returnA -< Recommended { .. }
